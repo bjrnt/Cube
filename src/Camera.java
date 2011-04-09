cc88377fd45dae51948c430a6f619d71d8a93a7e
Bjorn Tegelund <b.tegelund@gmail.com>
2011-04-09 15:08:55 +0200

    Fixed perspective projection

--- src/Camera.java
+++ src/Camera.java
@@ -1,41 +1,15 @@
 /**
- * A Camera is used as a point of perspective when rendering objects within the game.
+ * The Camera's position is used as the projection plane.
  * 
  * @author Bjorn Tegelund
  */
 public class Camera extends GameObject {
-	public double rotationX, rotationY, rotationZ;
-	
 	/**
 	 * Creates a new Camera which is used to see objects in 3D space within the game.
 	 * @param pos the position of the camera.
-	 * @param rotX the X-rotation of the camera.
-	 * @param rotY the Y-rotation of the camera.
-	 * @param rotZ the Z-rotation of the camera.
 	 */
-	public Camera(Vector3D pos, double rotX, double rotY, double rotZ) {
+	public Camera(Vector3D pos) {
 		super(pos);
-		rotationX = rotX;
-		rotationY = rotY;
-		rotationZ = rotZ;
 	}
 	
-	/**
-	 * @return the Camera's X-rotation.
-	 */
-	public double getRotX() {
-		return rotationX;
-	}
-	/**
-	 * @return the Camera's Y-rotation.
-	 */
-	public double getRotY() {
-		return rotationY;
-	}
-	/**
-	 * @return the Camera's Z-rotation.
-	 */
-	public double getRotZ() {
-		return rotationZ;
-	}
 }
--- src/Translator.java
+++ src/Translator.java
@@ -1,53 +1,16 @@
 public class Translator {
 	Camera camera;
+	Vector3D centerOfProjection;
 	
 	public Translator(Camera camera) {
 		this.camera = camera;
+		centerOfProjection = new Vector3D(camera.getX()+100,camera.getY(),camera.getZ());
 	}
 	
 	public Vector2D translateTo2D(Vector3D v3d) {
-		Vector3D eye = new Vector3D(600,0,0); //Experimental use
-		
-		float x = (
-				(float)
-				(Math.cos(camera.getRotY()) 
-				* ((v3d.getY() - camera.getY())
-				+ Math.cos(camera.getRotZ())
-				* (v3d.getX() - camera.getX()))
-				- Math.sin(camera.getRotY())
-				* (v3d.getZ() - camera.getZ())));
-		
-		float y = (
-				(float)
-				(Math.sin(camera.getRotX()) 
-				* (Math.cos(camera.getRotY()) 
-						* (v3d.getZ() - camera.getZ())
-						+ Math.sin(camera.getRotY()) 
-						* (Math.sin(camera.getRotZ())
-								* (v3d.getY() - camera.getY())
-						+ Math.cos(camera.getRotZ())
-						* (v3d.getX() - camera.getX())))
-				+ Math.cos(camera.getRotX())
-				* (Math.cos(camera.getRotZ())
-						* (v3d.getY() - camera.getY())
-						- Math.sin(camera.getRotZ())
-						* (v3d.getX() - camera.getX()))));
-		float z = (
-				(float)
-				(Math.cos(camera.getRotX()) 
-				* (Math.cos(camera.getRotY()) 
-						* (v3d.getZ() - camera.getZ())
-						+ Math.sin(camera.getRotY()) 
-						* ((v3d.getY() - camera.getY())
-						+ Math.cos(camera.getRotZ())
-						* (v3d.getX() - camera.getX())))
-				- Math.sin(camera.getRotX())
-				* (Math.cos(camera.getRotZ()
-						* (v3d.getY() - camera.getY())
-						- Math.sin(camera.getRotZ())
-						* (v3d.getX() - camera.getX())))));
-		
-		System.out.println(x + "  " + y + "  " + z);
-		return new Vector2D((y-eye.getY()*(eye.getX()/x)), (z-eye.getZ()*(eye.getX()/x)));
+		float w = v3d.getX()/(centerOfProjection.getX() - camera.getX()) + 1;
+		float z = v3d.getZ()/w;
+		float y = v3d.getY()/w;
+		return new Vector2D(y,z);
 	}
 }
