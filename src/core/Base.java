1b709de956d75c97596a8df79de1db2119aadf53
Bjorn Tegelund <b.tegelund@gmail.com>
2011-04-25 12:59:05 +0200

    Added in-game UI and improved menu/UI code

--- src/core/Base.java
+++ src/core/Base.java
@@ -5,8 +5,8 @@ import game.GameController;
 
 import java.util.ArrayList;
 
-import menu.Menu;
-import menu.MenuItem;
+import ui.UI;
+import ui.UIButton;
 import objectTypes.Vector2D;
 import objectTypes.Vector3D;
 
@@ -38,7 +38,7 @@ public class Base extends BasicGame {
 	private InputHandler ih;
 	private CubeController cc;
 	private GameController gc;
-	private Menu menu;
+	private UI ui;
 	private Game game;
 	
 	private Cube c;
@@ -62,21 +62,26 @@ public class Base extends BasicGame {
 	@Override
 	public void init(GameContainer container) throws SlickException {
 		container.setVSync(true);
-		// Game game = new Game(); For when the Game class exists
 		
 		background = new Image("data/bg.jpg");
+		
 		c = new Cube(10);
 		s = c.getSquare(0, 0, 0);
+		
 		input = container.getInput();
 		cc = new CubeController(c);
 		
 		game = new Game(c.getSquare(0, 0, 0));
 		gc = new GameController(game);
 		
-		menu = new Menu(input);
-		menu.enableMenuClick();
+		ui = new UI(input);
+		ui.loadMenuButtons();
+		ui.enableClick();
 	}
 	
+	/**
+	 * Enables the use of the keyboard to control the rotation of the Cube and the selected square.
+	 */
 	private void enableCubeInput() {
 		ih = new InputHandler(input, cc, gc);
 	}
@@ -90,15 +95,23 @@ public class Base extends BasicGame {
 		
 		background.draw(0,0);
 		
-		if(menu.getActive()) {
-			ArrayList<MenuItem> menuItems = menu.getMenuItems();
-			for(int i = 0; i < menuItems.size(); i++) {
-				menuItems.get(i).getImage().draw(menuItems.get(i).getX(), menuItems.get(i).getY());
-			}
+		if(ui.getMenuActive()) {
+			renderUI();
 			return;
 		}
 		
 		renderCube(g);
+		renderUI();
+	}
+	
+	/**
+	 * Renders all the currently active buttons in the UI.
+	 */
+	private void renderUI() {
+		ArrayList<UIButton> buttons = ui.getButtons();
+		for(int i = 0; i < buttons.size(); i++) {
+			buttons.get(i).getImage().draw(buttons.get(i).getX(), buttons.get(i).getY());
+		}
 	}
 	
 	/**
@@ -126,8 +139,9 @@ public class Base extends BasicGame {
 		renderManySquares(c.getSquares(mt), g, mt);
 
 		// rendering arrows
-		if(ih.usingPrimaryKeySet())
-			drawLines(g, mt, c.getArrows(mt), Color.red);
+		if(ih != null)
+			if(ih.usingPrimaryKeySet())
+				drawLines(g, mt, c.getArrows(mt), Color.red);
 
 		// Rendering grid
 		drawLines(g, mt, c.getGrid(mt), Color.lightGray);
@@ -145,13 +159,6 @@ public class Base extends BasicGame {
 		}
 	}
 
-	/**
-	 * Renders a square to the screen
-	 * 
-	 * @param g
-	 * @param mt
-	 * @param s
-	 */
 	private void renderSquare(Graphics g, MatrixTranslator mt, Square s) {
 		g.setColor(s.getBackColor());
 		g.fill(mt.translateSquare(s));
@@ -170,11 +177,13 @@ public class Base extends BasicGame {
 	@Override
 	public void update(GameContainer container, int delta)
 			throws SlickException {
-		if(menu.getActive()) {
+		if(ui.getMenuActive()) {
 			return;
 		}
-		if(ih == null)
+		if(ih == null) {
 			enableCubeInput();
+			ui.loadIngameButtons();
+		}
 		
 		cc.runAnimation();
 		game.update(delta);
---
+++ src/inputListeners/ButtonClickMouse.java
@@ -0,0 +1,78 @@
+package inputListeners;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.Input;
+import org.newdawn.slick.MouseListener;
+
+import ui.UI;
+import ui.UIButton;
+
+/**
+ * Used to listen after clicks on MenuItems in the Menu.
+ * @author Björn Tegelund
+ */
+public class ButtonClickMouse implements MouseListener {
+	private UI ui;
+	private boolean isAccepting;
+	private ArrayList<UIButton> buttons;
+
+	/**
+	 * Creates a new mouse listener listening for clicks on MenuItems in the Menu.
+	 */
+	public ButtonClickMouse(UI ui) {
+		this.ui = ui;
+		this.buttons = ui.getButtons();
+		isAccepting = true;
+	}
+
+	@Override
+	public void mouseClicked(int button, int x, int y, int clickCount) {
+		if(button != Input.MOUSE_LEFT_BUTTON)
+			return;
+		
+		for(int i = 0; i < buttons.size(); i++) { //Checks to see which item has been clicked
+			if(buttons.get(i).getRect().contains(x, y)) { 
+				ui.clicked(buttons.get(i));
+				return;
+			}
+		}
+	}
+	
+	@Override
+	public void inputEnded() {
+	}
+
+	@Override
+	public void inputStarted() {
+	}
+
+	@Override
+	public boolean isAcceptingInput() {
+		return isAccepting;
+	}
+
+	@Override
+	public void setInput(Input arg0) {
+	}
+
+	@Override
+	public void mouseDragged(int arg0, int arg1, int arg2, int arg3) {
+	}
+
+	@Override
+	public void mouseMoved(int arg0, int arg1, int arg2, int arg3) {
+	}
+
+	@Override
+	public void mousePressed(int arg0, int arg1, int arg2) {
+	}
+
+	@Override
+	public void mouseReleased(int arg0, int arg1, int arg2) {
+	}
+
+	@Override
+	public void mouseWheelMoved(int arg0) {
+	}
+}
