889e6fc3825287cc11b1028a85d3b3a5180a292a
Bjorn Tegelund <b.tegelund@gmail.com>
2011-04-25 18:45:17 +0200

    Added documentation to animation classes

--- src/animation/AbstractAnimation.java
+++ src/animation/AbstractAnimation.java
@@ -1,27 +1,51 @@
 package animation;
 
+/**
+ * AbstractAnimation is used to provide some baseline functionality for any inheriting classes.
+ * Some examples of this is being able to check if the animation is currently running, start it, etc.
+ * @author Björn Tegelund
+ */
 public abstract class AbstractAnimation {
 	private boolean active;
 	private AnimationCaller caller;
 	
+	/**
+	 * @param caller
+	 */
 	protected AbstractAnimation (AnimationCaller caller) {
 		this.caller = caller;
 	}
 	
+	/**
+	 * Sets this animation to run.
+	 */
 	public void begin() {
 		active = true;
 	}
 	
+	/**
+	 * @return Boolean with a value describing if this animation is active or not.
+	 */
 	public boolean isActive() {
 		return active;
 	}
 	
+	/**
+	 * Runs a part of this animation, determined by how long time has passed since the last frame.
+	 * @param delta The amount of time that has passed since the last frame.
+	 */
 	abstract void update(int delta);
 	
+	/**
+	 * Stops this animation without completing it.
+	 */
 	public void stop() {
 		notifyCaller();
 	}
 	
+	/**
+	 * Notifies the caller that this animation either is complete or stopped.
+	 */
 	protected void notifyCaller() {
 		caller.animationDone(this);
 	}
